0bdcdac13f0dbef2aabb389651e7a5e13a80d06d
Eduardo Tongson <propolice@gmail.com>
2021-08-02 10:48:43 +0800

    Just -v from hereon.

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ func main() {
 	cli := clir.NewCli("iyclo", "Well-oiled containers", versionString)
 	cli.SetBannerFunction(customBanner)
 	var action actionT
-	cli.BoolFlag("version", "Show version", &action.version)
 	cli.BoolFlag("v", "Show version", &action.version)
 	cli.StringFlag("log", "Path to JSON log", &action.log)
 	cli.Action(handleCli(&action))
