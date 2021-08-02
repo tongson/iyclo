ccff090ecd824973fc727ada52a5294ce4c6398e
Eduardo Tongson <propolice@gmail.com>
2021-08-02 09:11:33 +0800

    Actually a cli test. Needs hacks to handle the os.Exit.

--- cli_test.go
+++ cli_test.go
@@ -1,8 +1,26 @@
 package main
 
-import ()
+import (
+	"bytes"
+	"os"
+	"os/exec"
+	"testing"
 
-func ExampleVersionString() {
-	printVersion()
-	// Output: 0.1.0 (Chill Hazelnut)
+	assert "github.com/stretchr/testify/assert"
+)
+
+func TestVersionString(t *testing.T) {
+	if os.Getenv("GOTEST") == "1" {
+		printVersion()
+	}
+
+	cmd := exec.Command(os.Args[0], "-test.run=TestVersionString")
+	cmd.Env = append(os.Environ(), "GOTEST=1")
+	var stdout bytes.Buffer
+	cmd.Stdout = &stdout
+	err := cmd.Run()
+	_, ok := err.(*exec.ExitError)
+	assert.Equal(t, false, ok)
+	expectedString := "0.1.0 (Chill Hazelnut)\n"
+	assert.Equal(t, expectedString, stdout.String())
 }
