1b0a2a48f4b1c8cf5f6acddc83e03e0c74156e03
Eduardo Tongson <propolice@gmail.com>
2021-08-02 13:42:52 +0800

    Initial implementation of HTTP handling code.

---
+++ http.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"embed"
+	"net"
+	"net/http"
+
+	echo "github.com/labstack/echo/v4"
+	middleware "github.com/labstack/echo/v4/middleware"
+	ll "github.com/tongson/LadyLua"
+	glecho "github.com/tongson/iyclo/internal/glecho"
+	lua "github.com/yuin/gopher-lua"
+)
+
+//go:embed lua/*
+var luaSrc embed.FS
+
+func mainHttp(l net.Listener) {
+	e := echo.New()
+	e.Listener = l
+	server := new(http.Server)
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.GET("/*", handleHttp)
+	e.Logger.Fatal(e.StartServer(server))
+}
+
+func handleHttp(c echo.Context) error {
+	L := lua.NewState()
+	defer L.Close()
+	L.SetMx(256)
+	glecho.Load(L, c) // Loads global E
+	ll.Preload(L)     // Load embedded Lua modules
+	return ll.Main(L, ll.ReadFile(luaSrc, "lua/main.lua"))
+}
---
+++ lua/main.lua
@@ -0,0 +1,3 @@
+E:response():write("test")
+E:response():status(200)
+E:response():flush()
