c2bbc95e0dfd0f63e7ee228df178ac3057d6f771
Eduardo Tongson <propolice@gmail.com>
2021-08-03 18:26:59 +0800

    Found cleaner way to declare these.

--- http_test.go
+++ http_test.go
@@ -7,20 +7,26 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
+	"github.com/rs/zerolog"
 )
 
-func TestRoute(t *testing.T) {
-	vars := make(map[string]string)
-	vars["db"] = "/tmp"
-	jl := jLog("/dev/null")
-	e := echo.New()
-	expected := `ok`
+var t_variables map[string]string
+var t_logger zerolog.Logger
 
+func init() {
+	t_variables = make(map[string]string)
+	t_variables["db"] = "/tmp"
+	t_logger = jLog("/dev/null")
+}
+
+func TestRoute(t *testing.T) {
+	e := echo.New()
 	req := httptest.NewRequest(http.MethodGet, "/api/v1/containers", nil)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
-	h := handleHttp(jl, vars)
+	h := handleHttp(t_logger, t_variables)
 	if assert.NoError(t, h(c)) {
+		expected := `ok`
 		assert.Equal(t, http.StatusAccepted, rec.Code)
 		assert.Equal(t, expected, rec.Body.String())
 	}
