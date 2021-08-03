09c15f90f26d0be656a342c9c36d91b339c28826
Eduardo Tongson <propolice@gmail.com>
2021-08-03 17:27:31 +0800

    This is a decision table.

--- src/main.lua
+++ src/main.lua
@@ -2,6 +2,7 @@ local tuple = require("tuple")
 local handler = require("handler")
 local request = tuple(E:request():method(), E:request():uri())
 local arrived = tostring(request)
+-- Decision Table
 -- (method, path)
 local entrypoints = {
 	["(GET, /api/v1/containers)"] = "get_containers"
