a39811e590b09654c37e2dd64b185bb5e07b1ddf
Eduardo Tongson <propolice@gmail.com>
2021-08-04 13:33:42 +0800

    No all caps.

--- src/main.lua
+++ src/main.lua
@@ -1,6 +1,6 @@
 local tuple = require("tuple")
 local crypto = require("crypto")
-local ID = crypto.fast_random()
+local reqid = crypto.fast_random()
 local handler = require("handler")
 local request = tuple(E:request():method(), E:request():uri())
 local signature = tostring(request)
@@ -11,8 +11,8 @@ local entrypoints = {
 }
 local matched = entrypoints[signature]
 if matched then
-	L:info(ID, { sig = signature, fn = matched })
-	return handler[matched](ID)
+	L:info(reqid, { sig = signature, fn = matched })
+	return handler[matched](reqid)
 else
 	E:response():write_header(404)
 	E:response():flush()
